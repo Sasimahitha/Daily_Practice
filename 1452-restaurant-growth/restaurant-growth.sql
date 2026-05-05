1d2f105c87771780a956ab1b9d093aa068674cc6
Sasimahitha <105411649+Sasimahitha@users.noreply.github.com>
2026-05-05 14:48:59 +0530

    Time: 379 ms (25.75%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,8 +1,3 @@
-# Write your MySQL query statement below
--- select visited_on, sum(amount) over(rows between 6 preceding and current row) as amt
--- from customer
--- group by visited_on;
-
 select t1.visited_on, sum(t1.tot) over(rows between 6 preceding and current row) as amount, round(avg(t1.tot) over(rows between 6 preceding and current row),2) as average_amount
 from (
 select visited_on, sum(amount) as tot
